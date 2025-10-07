67d07aa249fa89f2ed7e17e63a3421d0a2d1e70a
MeryemHafedhAhmed <148851192+MeryemHafedhAhmed@users.noreply.github.com>
2025-10-07 15:24:41 +0300

    Add files via upload

---
+++ 2.HAFTA.cpp
@@ -0,0 +1,49 @@
+//1 Algo
+for (i = 1; i <= n; i++) {
+    print("Hello World");
+}//O(n) 
+
+
+
+//2 Algo
+for (i = 1; i <= n; i++) {
+    for (j = 1; j <= n; j++) {
+        print("Hello World");
+    }
+}//O(n²)
+
+
+
+// 3 Algo
+for (i = 1; i < n; i = i * 2) {
+    print("Hello World");
+}//O(log n)
+
+
+
+//4 Algo
+for (i = 1; i < n; i = i * 5) {
+    print("Hello World");
+}//O(log n)
+
+
+//5 Algo
+for (i = 1; i < n³; i = i * 5) {
+    print("Hello World");
+}//O(log n)
+
+
+//6 Algo
+for (i = 1; i * i <= n; i++) {
+    print("Hello World");
+}//O(n^1/2)
+
+
+
+//7 Algo
+i = 1;
+k = 1;
+while (k <= n) {
+    i = i + 1;
+    k = k + i;
+}//O(n^1/2)
